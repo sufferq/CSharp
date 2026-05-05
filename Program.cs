e57c8ab045378ad6bc6c512f847a43eb7114b283
sufferq <semenbulavin@yandex.ru>
2026-05-05 15:44:07 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите число B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            if (A > B)
+            {
+                Console.WriteLine("Число A не может быть больше числа B!");
+            }
+            else
+            {
+                for (int i = A; i <= B; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write(i);
+                    }
+                    Console.WriteLine(" ");
+                }
+            }
+        }
+    }
+}
