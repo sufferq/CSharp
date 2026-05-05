b7e6140922c1ce97641b536caf80f36554a58dd0
sufferq <semenbulavin@yandex.ru>
2026-05-05 16:22:59 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите число N(больше нуля): ");
+            string N = Console.ReadLine();
+
+            char[] nums = N.ToCharArray();
+            Array.Reverse(nums);
+
+            Console.WriteLine(nums);
+        }
+    }
+}
