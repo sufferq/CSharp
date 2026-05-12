ac5ed2c6f95564a219b28ee95979e764c579a70e
sufferq <semenbulavin@yandex.ru>
2026-05-12 12:42:08 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 6, 0, -2, 4, -4, -6, 1 };
+
+            Array.Sort(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }  
+        }
+    }
+}
