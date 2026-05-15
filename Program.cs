98d86f6f5cb5299de9757f0bc662e7011ecdc194
sufferq <semenbulavin@yandex.ru>
2026-05-15 14:00:17 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,36 +1,101 @@
-﻿namespace Project3___1._4
+﻿namespace ConsoleApp1
 {
-    class Notebook() {
 
-        public string model = string.Empty;
-        public string processorClockFrequency = string.Empty;
-        public string RAM = string.Empty;
-        public string hardDrive = string.Empty;
-        public string weight = string.Empty;
+    class FeaturesBooks
+    {
 
-        public void Info() {
+        private string name = string.Empty;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
 
-            Console.WriteLine("Инфо о ноутбуке: ");
-            Console.WriteLine("Модель: " + model + ","
-                + " Тактовая частота процессора: " + processorClockFrequency + ", "
-                + "Объем оперативной памяти: " + RAM + ", "
-                + "Объем жесткого диска: " + hardDrive
-                + "Масса: " + weight);
+        private string autor = string.Empty;
+        public string Autor
+        {
+            get { return autor; }
+        }
+
+        public string PublishingHouse { get; private set; } = string.Empty;
+
+        public int yearOfRelease;
+        public int pageCount;
+
+
+        public FeaturesBooks(string name, string autor, string publishingHouse, int yearOfRelease, int pageCount)
+        {
+            this.name = name;
+            this.autor = autor;
+            PublishingHouse = publishingHouse;
+            this.yearOfRelease = yearOfRelease;
+            this.pageCount = pageCount;
+        }
+
+
+        public string Info()
+        {
+
+            return $"Название: {name}, " +
+                   $"Автор: {autor}, " +
+                   $"Год выпуска: {yearOfRelease}, " +
+                   $"Объем: {pageCount} листов, " +
+                   $"Издательство: {PublishingHouse}";
+
+        }
+    }
+
+    class MethodBooks
+    {
+        public string name = string.Empty;
+        public string autor = string.Empty;
+        public string publishingHouse = string.Empty;
+        public int yearOfRelease;
+        public int pageCount;
+
+
+        public MethodBooks(string name, string autor, string publishingHouse, int yearOfRelease, int pageCount)
+        {
+            this.name = name;
+            this.autor = autor;
+            this.publishingHouse = publishingHouse;
+            this.yearOfRelease = yearOfRelease;
+            this.pageCount = pageCount;
+        }
+        public string Info()
+        {
+            return $"Название: {name}, " +
+                   $"Автор: {autor}, " +
+                   $"Год выпуска: {yearOfRelease}, " +
+                   $"Объем: {pageCount} листов, " +
+                   $"Издательство: {publishingHouse}";
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Notebook notebook = new Notebook();
+            FeaturesBooks featuresBooks = new FeaturesBooks("Война и мир", "Лев Толстой", "АСТ", 1869, 1300);
+            MethodBooks methodBooks = new MethodBooks("Война и мир", "Лев Толстой", "АСТ", 1869, 1300);
+            Console.Write("Со свойставами: ");
+            Console.WriteLine(featuresBooks.Info());
+            Console.WriteLine("\n");
+            Console.Write("Без свойств: ");
+            Console.WriteLine(methodBooks.Info());
+            Console.WriteLine("\n");
 
-            notebook.model = "Apple MacBook Air M2";
-            notebook.processorClockFrequency = "3.49 ГГц";
-            notebook.RAM = "16 ГБ";
-            notebook.hardDrive = "512 ГБ SSD";
-            notebook.weight = "1.24 кг";
+            featuresBooks.Name = "Анна Каренина";
+            //featuresBooks.Autor = "New autor"; Будет ошибка! Так как он только для чтения
+            Console.Write("После изменения первого класса со свойствами: ");
+            Console.WriteLine(featuresBooks.Info());
+            Console.WriteLine("\n");
 
-            notebook.Info();
+            methodBooks.name = "Идиот"; // Можем изменять напрямую
+            methodBooks.autor = "Достоевский";
+            Console.Write("После изменения второго класса без свойств: ");
+            Console.WriteLine(methodBooks.Info());
+
+
         }
     }
 }
